472a999ed93fa93038261866876e354fbaaab38c
panguangze <guangzep@gmail.com>
2021-07-12 05:20:11 +0800

    reflect

--- reflect_back.py
+++ reflect_back.py
@@ -20,6 +20,7 @@ def main():
     #chromos=["1"]
     chromos = c_vcf.contigs.keys()
     hete_infos = {}
+    print('fectch')
     for chromo in chromos:
         tmp= {}
         try:
@@ -28,9 +29,9 @@ def main():
             continue
         for rec in c_vcf.fetch(chromo):
             try:
-                tmp[rec.POS]=[str(rec.samples[0]['GT']),str(rec.samples[0]['PS'])]
+                tmp[str(rec.POS)+str(rec.ALT[0])]=[str(rec.samples[0]['GT']),str(rec.samples[0]['PS'])]
             except:
-                tmp[rec.POS]=[str(rec.samples[0]['GT']),"0"]
+                tmp[str(rec.POS)+str(rec.ALT[0])]=[str(rec.samples[0]['GT']),"0"]
         hete_infos[chromo] = tmp
 
     for line in orig_vcf.readlines():
@@ -39,12 +40,13 @@ def main():
         a = line.split("\t")
         chromo = a[0]
         pos = a[1]
+        alt = a[4].split(',')
         snp_format = a[-2]
         if "PS" not in snp_format:
             a[-2] = a[-2]+":PS"
         vs = a[-1].split(":")
-        if int(pos) in hete_infos[chromo].keys():
-            target = hete_infos[chromo][int(pos)]
+        if str(pos)+alt[0] in hete_infos[chromo].keys():
+            target = hete_infos[chromo][str(pos)+alt[0]]
             if len(vs) != 3:
                 vs.append(target[1])
             else:
@@ -59,6 +61,28 @@ def main():
         res = '\t'.join(a)
         res = res.replace('\r', '').replace('\n', '')
         out_vcf.write(res+"\n")
+
+    # # for chromo in chromos:
+    #     print(chromo)
+    #     try:
+    #         orig_vcf.fetch(chromo)
+    #     except:
+    #         print(chromo)
+    #         continue
+    #     for rec in orig_vcf.fetch(chromo):
+    #         if rec.POS in hete_infos[chromo].keys():
+    #             try:
+    #                 rec.add_format('PS')
+    #                 samp_fmt = vcf.model.make_calldata_tuple(rec.FORMAT.split(':'))
+    #             except:
+    #                 pass
+    #             tmp = rec.samples[0].data._asdict()
+    #             tmp['GT'] = hete_infos[chromo][rec.POS][0]
+    #             tmp['PS'] = hete_infos[chromo][rec.POS][1]
+    #             rec.samples[0].data = samp_fmt(**tmp)
+    #             # rec.samples[0]['GT'] = hete_infos[chromo][rec.POS][0]
+    #             # rec.samples[0]['PM'] = hete_infos[chromo][rec.POS][0]
+    #         out_vcf.write_record(rec)
 
 if __name__ == "__main__":
     main() 
